586e9c713ceb972454ede188aca773b012c4c645
hitoshiA <anjihitoshi@gmail.com>
2025-07-29 23:24:26 +0900

    KB ID を変更

--- packages/cdk/parameter.ts
+++ packages/cdk/parameter.ts
@@ -23,7 +23,7 @@ const envs: Record<string, Partial<StackInput>> = {
   dev: {
     // Parameters for development environment
     ragKnowledgeBaseEnabled: true,
-    ragKnowledgeBaseId: 'XXXXXXXXXX',
+    ragKnowledgeBaseId: '',
     ragKnowledgeBaseStandbyReplicas: false,
     ragKnowledgeBaseAdvancedParsing: true,
     ragKnowledgeBaseAdvancedParsingModelId:
